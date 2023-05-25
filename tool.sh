ea26f570cc64de493462b863c686edbcdef82e1c
Pratik Pohankar <51027461+alastair66@users.noreply.github.com>
2023-05-25 10:04:20 -0400

    Add files via upload
    execute the script

---
+++ tool.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+url=$1
+
+if [ ! -d "$url" ];then
+	mkdir $url
+fi
+
+if [ ! -d "$url/subs" ];then
+	mkdir $url/recon
+fi
+
+
+echo "[+] Scanning for sub-domains..."
+assetfinder --subs-only $url >> $url/recon/target.txt
+cat $url/recon/target.txt | grep $1 >> $url/recon/final.txt
+echo "[+] Scanning for alive subs..."
+cat $url/recon/final.txt | httprobe >> $url/recon/alive-subs.txt
+rm $url/recon/target.txt
+
